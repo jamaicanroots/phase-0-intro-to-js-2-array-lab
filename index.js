a377cf8463343f99cddd7f4478a06e6b39acd2bc
matthewelam <ematew418@gmail.com>
2022-07-02 11:14:31 -0400

    commit tree 8acf8b86f111cbf7334d3c73515dbb2f2b34e235

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+// Write your solution here
+
+const cats = ["Milo", "Otis", "Garfield"]
+const allcats = [...cats, "Broom"]
+const allthemcats = ["Arnold", ...cats]
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat() {
+    return allcats;
+}
+function prependCat() {
+    return allthemcats
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
